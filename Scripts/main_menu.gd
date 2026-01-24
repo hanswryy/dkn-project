43730967b27a2112e5f3b2e6f17ac91891846a8a
Dimastio Setiawan <dimastio20062@gmail.com>
2026-01-24 10:38:06 +0700

    add settings menu for master & music volume; add music & sfx audio buses

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -4,18 +4,44 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = true
+	AudioFade.fade_in($BGM, 2)
+	get_tree().paused = false
+	$Settings/MasterVol/Value.text = str(int(UserSettings.master_volume * 10))
+	$Settings/MusicVol/Value.text = str(int(UserSettings.music_volume * 10))
 
 func _on_button_2_pressed() -> void:
-	fade_out($BGM, 4)
+	AudioFade.fade_out($BGM, 4)
 	FadeToBlack_Transition.fade_to_scene("res://Scenes/world.tscn", 4)
 
 func _on_button_3_pressed() -> void:
-	pass # Replace with function body.
+	$Settings.visible = true
+	$ColorRect/Main.visible = false
 
 func _on_button_4_pressed() -> void:
 	get_tree().quit()
 
-func fade_out(player: AudioStreamPlayer, duration := 1.0):
-	var tween := create_tween()
-	tween.tween_property(player, "volume_db", -80, duration)
-	tween.finished.connect(player.stop)
+func _on_button_5_pressed() -> void:
+	$Settings.visible = false
+	$ColorRect/Main.visible = true
+	UserSettings.apply_settings()
+	UserSettings.save_settings()
+
+func _on_dec_pressed() -> void:
+	if UserSettings.master_volume > 0:
+		UserSettings.master_volume -= 0.1
+		$Settings/MasterVol/Value.text = str(int(UserSettings.master_volume * 10))
+
+func _on_inc_pressed() -> void:
+	if UserSettings.master_volume < 1.0:
+		UserSettings.master_volume += 0.1
+		$Settings/MasterVol/Value.text = str(int(UserSettings.master_volume * 10))
+
+func _on_dec_2_pressed() -> void:
+	if UserSettings.music_volume > 0:
+		UserSettings.music_volume -= 0.1
+		$Settings/MusicVol/Value.text = str(int(UserSettings.music_volume * 10))
+
+func _on_inc_2_pressed() -> void:
+	if UserSettings.music_volume < 1.0:
+		UserSettings.music_volume += 0.1
+		$Settings/MusicVol/Value.text = str(int(UserSettings.music_volume * 10))
