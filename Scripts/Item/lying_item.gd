9e68b72b64ed844c36fa3bf533bdc5580ae85944
AzkaLufthansa <azkalufthansa3@gmail.com>
2026-01-26 18:18:35 +0700

    fix pickup item mechanism

--- Scripts/Item/lying_item.gd
+++ Scripts/Item/lying_item.gd
@@ -11,11 +11,16 @@ func _ready():
 	input_event.connect(_on_input_event)
 	mouse_entered.connect(_on_mouse_highlight)
 	mouse_exited.connect(_off_mouse_highlight)
-
+	
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int):
 	if event is InputEventMouseButton and event.pressed and !get_tree().paused:
 		if event.button_index == MOUSE_BUTTON_LEFT:
-			pick_up()
+			# tell player to start moving to item position
+			var player = get_tree().get_first_node_in_group("player")
+			if player:
+				player.target_item = self
+				print("Target Item Coord: " + str(player.target_item))
+				player.get_coord_to_pos(global_position)
 
 func pick_up():
 	if item:
