d92d30db737db83e83154a40ec781151f21bc156
Yoga Akbar Nugraha <yogaakbar1205@gmail.com>
2026-01-27 16:01:59 +0700

    level design dining room
     creating dining room level design

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,6 +6,7 @@ var current_path_id: Array[Vector2i]
 var speed := 50
 
 var last_frame : int = -1
+
 var target_item : Area2D = null
 
 func _ready():
@@ -18,18 +19,22 @@ func _unhandled_input(event):
 		target_item = null
 		get_coord()
 
+# move to spesific coord
 func get_coord_to_pos(target_pos: Vector2):
 	var start_point = Tile_map.local_to_map(global_position)
+	print("start_point" + str(start_point))
 	var end_point = Tile_map.local_to_map(target_pos)
+	print("end_point: " + str(end_point))
 	current_path_id = Astar.get_id_path(start_point, end_point).slice(1)
+	print("current_path_id: " + str(current_path_id))
 
 func get_coord():
 	var start_point = Tile_map.local_to_map(global_position)
-	var end_point = Tile_map.local_to_map(get_global_mouse_position())
+	var end_point   = Tile_map.local_to_map(get_global_mouse_position())
 	current_path_id = Astar.get_id_path(start_point, end_point).slice(1)
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_down"):
+	if (Input.is_action_just_pressed("ui_down")):
 		die()
 	
 	if current_path_id.is_empty():
@@ -37,7 +42,9 @@ func _process(delta):
 		$Langkah.stop()
 		last_frame = -1
 		
+		# pickup the item when arrived at destination
 		if target_item != null:
+			print("Distance: " + str(global_position.distance_to(target_item.global_position)))
 			if global_position.distance_to(target_item.global_position) < 32:
 				target_item.pick_up()
 				target_item = null
@@ -58,15 +65,13 @@ func _process(delta):
 
 func move_playerTo(target, delta):
 	var direction = (target - global_position).normalized()
-	
-	# >>> GERAK NORMAL (tanpa raycast)
-	velocity = direction * speed
+	global_position += direction * speed * delta
 	move_and_slide()
 
 	if abs(direction.x) > abs(direction.y):
 		$AnimatedSprite2D.play("w_right" if direction.x > 0 else "w_left")
 	else:
-		$AnimatedSprite2D.play("w_down" if direction.y > 0 else "w_up")
+		$AnimatedSprite2D.play("w_down"  if direction.y > 0 else "w_up")
 	
 	play_footstep_sound()
 
@@ -74,8 +79,10 @@ func play_footstep_sound():
 	var current_frame = $AnimatedSprite2D.frame
 	
 	if current_frame != last_frame:
+		# >>> play suara di frame 1 dan 3
 		if current_frame == 1 or current_frame == 3:
 			$Langkah.play()
+		
 		last_frame = current_frame
 
 func die():
