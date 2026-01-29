0ba94b7714b46aba5d08b1f17f69d69a60b411dd
unknown <yogaakbar1205@gmail.com>
2026-01-29 13:33:40 +0700

    fix-jumpscare

--- Scripts/jumpscare.gd
+++ Scripts/jumpscare.gd
@@ -51,6 +51,7 @@ func setup_trigger():
 
 # >>> TRIGGER: Player masuk Area2D
 func _on_trigger_body_entered(body):
+	print("ez")
 	if body.name == "Player":
 		# >>> Langsung trigger jika belum pernah
 		if not has_been_triggered and not is_jumpscare_active:
